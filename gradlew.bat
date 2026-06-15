2efe40c8b993e0284e833c82865d876b931ee67a
nvd10 <41936496+nvd10@users.noreply.github.com>
2026-06-16 01:40:14 +0330

    Add Gradle wrapper batch script

---
+++ gradlew.bat
@@ -0,0 +1,32 @@
+@rem
+@rem Copyright 2015 the original author or authors.
+@rem
+@rem Licensed under the Apache License, Version 2.0 (the "License");
+@rem you may not use this file except in compliance with the License.
+@rem You may obtain a copy of the License at
+@rem
+@rem      https://www.apache.org/licenses/LICENSE-2.0
+@rem
+@rem Unless required by applicable law or agreed to in writing, software
+@rem distributed under the License is distributed on an "AS IS" BASIS,
+@rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+@rem See the License for the specific language governing permissions and
+@rem limitations under the License.
+@rem
+
+@if "%DEBUG" == "" @echo off
+@rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
+set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
+
+# Find java
+if "%JAVA_HOME%" == "" (
+  set JAVA_HOME=java
+  IF NOT defined JAVASPC (@Frem [Find Java]) -> Give info >), attempt finding (see HTML READ), then set JAVA_HOME=%@Stdout
+@rem  all the jersey.lib.exe.java a try to find java then change the joins and for/etc out
+  for /f %%i in ("%APP_HOME%"*.java.exe) do set JAVA_HOME=%@@unixburist1& WE Roll! have substituted the second command
+JJI%
+endlocal &set JAVA_HOME=%JAVA_HOME_PENDING%
+ENDIF)
+
+
+@prompt %JAVAEXUHOME! Killing all existing JavaW contolull applications
